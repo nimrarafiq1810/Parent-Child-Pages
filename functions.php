8aeb550f0bc88d73c8648f071c88ec8278f735e6
fhd-codes <m@ms-MacBook-Pro.local>
2023-09-29 21:51:12 +0500

    added ob_start etc

--- functions.php
+++ functions.php
@@ -18,14 +18,14 @@ add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 
 function enqueue_scripts() {
 
-    wp_enqueue_script('form-control-script', get_stylesheet_directory_uri() . '/influencers_search_tool/assets/js/form-control.js', array('jquery'), null, true);
+    wp_enqueue_script('form-control-script', get_stylesheet_directory_uri() . '/influencers_search_tool/assets/js/form-control.js', array('jquery', 'select2'), null, true);
     wp_enqueue_script('search-script', get_stylesheet_directory_uri() . '/influencers_search_tool/assets/js/search.js', array('jquery', 'select2'), null, true);
   
     wp_localize_script('form-control-script', 'localized_data', array(
       'not_found_icon' => get_stylesheet_directory_uri() . "/influencers_search_tool/assets/images/notFound.svg", // WordPress AJAX URL
     ));
 
-    // wp_enqueue_script('jquerymin', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), '3.6.0', true);
+    wp_enqueue_style('select2-css', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css', array(), '4.0.13');
     wp_enqueue_script('select2', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js', array(), '4.0.13', true);
 
   
